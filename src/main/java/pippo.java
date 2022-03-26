2b137ec13346ed91e9fca506a246f9b698cc4e92
kobero98 <matteo.fede@live.it>
2022-03-26 14:50:52 +0100

    test 2 CI with github action

--- src/main/java/pippo.java
+++ src/main/java/pippo.java
@@ -1,6 +1,12 @@
 public class pippo {
+    static public void stampa(){
+        System.out.print("ciao");
+        return ;
+    }
     public static void main(String[] args){
         System.out.println("Hello Word");
         System.out.println(12);
+        stampa();
     }
+
 }
---
+++ src/test/java/Testpippo.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Testpippo {
+    @Test
+    public void Teststampa(){
+        Assert.assertTrue(true);
+    }
+}
